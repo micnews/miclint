4fd14c17cfa1dd0dc34ee3508642bab48092b387
Roilan <roilan14@gmail.com>
2016-08-08 12:12:31 -0400

    initial linter with cli

---
+++ index.js
@@ -0,0 +1,4 @@
+var Linter = require('standard-engine').linter;
+var options = require('./options');
+
+module.exports = new Linter(options);
---
+++ options.js
@@ -0,0 +1,16 @@
+var path = require('path');
+var pkg = require('./package.json');
+
+
+module.exports = {
+  cmd: 'miclint',
+  version: pkg.version,
+  homepage: pkg.homepage,
+  bugs: pkg.bugs.url,
+  tagline: 'Mic lint',
+  eslint: require('eslint'),
+  eslintConfig: {
+    configFile: path.join(__dirname, 'eslintrc.json')
+  },
+  formatter: null
+};

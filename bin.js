1ebf32371312ce1aab72859d6e5c90efa6f90a7b
David Björklund <david.bjorklund@gmail.com>
2017-03-07 15:52:22 +0100

    miclint miclint

--- bin.js
+++ bin.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
+/* eslint-disable import/no-commonjs */
+const options = require('./options');
 
-var options = require('./options');
-var flow = process.argv.indexOf('--flow') !== -1;
+const flow = process.argv.indexOf('--flow') !== -1;
 
 require('standard-engine').cli(flow ? options.flow : options);
